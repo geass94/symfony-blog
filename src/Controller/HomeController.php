5771d9b5a117c43060ae4dd7491e7dc4b9921b5a
Otto Koridze <okoridze@yahoo.com>
2025-06-10 23:47:15 +0400

    wip

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -32,38 +32,4 @@ final class HomeController extends AbstractController
         ]);
     }
 
-    // src/Repository/PostRepository.php
-
-    public function findLatestPostsByCategories(array $categoryIds, int $limitPerCategory = 3): array
-    {
-        $conn = $this->em->getConnection();
-
-        $sql = '
-        SELECT p.*, pc.category_id
-        FROM post p
-        INNER JOIN post_category pc ON p.id = pc.post_id
-        WHERE pc.category_id IN (:categoryIds)
-        ORDER BY pc.category_id, p.created_at DESC
-    ';
-
-        $stmt = $conn->prepare($sql);
-        $stmt->executeQuery(['categoryIds' => $categoryIds]);
-
-        $rows = $stmt->fetchAllAssociative();
-
-        $postsByCategory = [];
-
-        foreach ($rows as $row) {
-            $catId = $row['category_id'];
-            if (!isset($postsByCategory[$catId])) {
-                $postsByCategory[$catId] = [];
-            }
-            if (count($postsByCategory[$catId]) < $limitPerCategory) {
-                $postsByCategory[$catId][] = $row;
-            }
-        }
-
-        return $postsByCategory;
-    }
-
 }
